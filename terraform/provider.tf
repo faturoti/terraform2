fcc1369c1200e7b132af120ea9066ba2fa24d7cb
Haider7214 <99068608+Haider7214@users.noreply.github.com>
2025-07-18 22:46:47 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 locals {
   region        = "ap-south-1"
-  name          = "telusko2-cluster"
+  name          = "telusko4-cluster"
   vpc_cidr      = "10.123.0.0/16"
   azs           = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
